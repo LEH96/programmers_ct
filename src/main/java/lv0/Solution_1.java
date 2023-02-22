42c4a2029783e0fc52be593132e8f95b25764882
LEH96 <lutea67@naver.com>
2023-02-22 15:01:42 +0900

    두수의 차

---
+++ src/main/java/lv0/Solution_1.java
@@ -0,0 +1,8 @@
+package lv0;
+
+public class Solution_1 {
+    public static int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static lv0.Solution_1.solution;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(solution(3,1));
+    }
+}
