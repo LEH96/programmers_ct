bab47b2dc6f2599960e82457e28fbaf6e907b408
LEH96 <lutea67@naver.com>
2023-02-23 09:20:51 +0900

    commit2 - add testcase

--- src/main/java/lv0/배열의평균값.java
+++ src/main/java/lv0/배열의평균값.java
@@ -2,7 +2,7 @@ package lv0;
 
 public class 배열의평균값 {
     public class Solution {
-        public double solution(int[] numbers) {
+        public static double solution(int[] numbers) {
             double sum = 0;
             for(int i=0;i<numbers.length;i++) {
                 sum+=numbers[i];
--- src/test/java/org/example/test.java
+++ src/test/java/org/example/test.java
@@ -1,5 +1,7 @@
 package org.example;
+
 import lv0.두수의차;
+import lv0.배열의평균값;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -8,6 +10,13 @@ public class test {
     @Test
     @DisplayName("3,2 => 1")
     void t1(){
-        assertThat(두수의차.Solution.solution(3, 2)).isEqualTo(1);
+        assertThat(두수의차.Solution.solution(3,2)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("[1,2,3] => 2")
+    void t2(){
+        int[] arr = {1,2,3};
+        assertThat(배열의평균값.Solution.solution(arr)).isEqualTo(2);
     }
 }
