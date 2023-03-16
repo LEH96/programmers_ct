bc6535f48c8f611551c8a053cd2186f9105c6024
LEH96 <lutea67@naver.com>
2023-03-16 17:36:33 +0900

    220316 commit

--- src/main/java/lv1/가장큰수찾기/Solution.java
+++ src/main/java/lv1/가장큰수찾기/Solution.java
@@ -1,4 +1,17 @@
 package lv1.가장큰수찾기;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Solution {
+    public int[] solution(int[] array) {
+        int[] result = new int[2];
+
+        result[0] = Arrays.stream(array).max().getAsInt();
+        result[1] = Arrays.stream(array)
+                            .boxed()
+                            .collect(Collectors.toList())
+                            .indexOf(result[0]);
+        return result;
+    }
 }
