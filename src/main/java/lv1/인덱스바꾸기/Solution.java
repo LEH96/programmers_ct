c1d0d99c014d3d2919f8edd819ef36718a5931b2
LEH96 <lutea67@naver.com>
2023-03-17 17:23:13 +0900

    230317 commit

--- src/main/java/lv1/인덱스바꾸기/Solution.java
+++ src/main/java/lv1/인덱스바꾸기/Solution.java
@@ -1,4 +1,15 @@
 package lv1.인덱스바꾸기;
 
+import java.util.Arrays;
+
 public class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] chars = my_string.toCharArray();
+
+        char tmp = chars[num1];
+        chars[num1] = chars[num2];
+        chars[num2] = tmp;
+
+        return new String(chars);
+    }
 }
--- src/main/java/lv1/피자나눠먹기2/Solution.java
+++ src/main/java/lv1/피자나눠먹기2/Solution.java
@@ -1,4 +1,10 @@
 package lv1.피자나눠먹기2;
 
 public class Solution {
+    public int solution(int n) {
+        int pizza = 1;
+        while((pizza * 6) % n != 0)
+            pizza += 1;
+        return pizza;
+    }
 }
