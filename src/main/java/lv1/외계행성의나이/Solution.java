c070be2e3624ba7eb8dd89662f66f6c822bffff4
LEH96 <lutea67@naver.com>
2023-03-20 17:22:48 +0900

    230320 commit

--- src/main/java/lv1/외계행성의나이/Solution.java
+++ src/main/java/lv1/외계행성의나이/Solution.java
@@ -1,4 +1,17 @@
 package lv1.외계행성의나이;
 
+import java.util.Arrays;
+
 public class Solution {
+    public String solution(int age) {
+        String answer = "";
+        char[] c = {'a','b','c','d','e','f','g','h','i','j'};
+
+        while(age > 0){
+            answer += c[age%10];
+            age = age / 10;
+        }
+
+        return new StringBuffer(answer).reverse().toString();
+    }
 }
