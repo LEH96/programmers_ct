012f3ad577a8e8710c20fc42d1e5bd156c8abff3
LEH96 <lutea67@naver.com>
2023-03-21 17:27:55 +0900

    230321 commit

--- src/main/java/lv1/게임369/Solution.java
+++ src/main/java/lv1/게임369/Solution.java
@@ -1,4 +1,16 @@
 package lv1.게임369;
 
 public class Solution {
+    public int solution(int order) {
+        int answer = 0;
+
+        while(order > 0){
+            if("369".contains(order%10+"")){
+                answer += 1;
+            }
+            order = order / 10;
+        }
+
+        return answer;
+    }
 }
--- src/main/java/lv1/숫자찾기/Solution.java
+++ src/main/java/lv1/숫자찾기/Solution.java
@@ -1,4 +1,15 @@
 package lv1.숫자찾기;
 
 public class Solution {
+    public int solution(int num, int k) {
+        String numbers = num + "";
+
+        for(int i=0;i<numbers.length();i++){
+            if(numbers.substring(i,i+1).equals(k+"")){
+                return i+1;
+            }
+        }
+
+        return -1;
+    }
 }
