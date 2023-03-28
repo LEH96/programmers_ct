a356959a865244757d827fd5116c8b162000f5f4
LEH96 <lutea67@naver.com>
2023-03-28 15:28:06 +0900

    220328 commit

--- src/main/java/lv1/한번만등장한문자/Solution.java
+++ src/main/java/lv1/한번만등장한문자/Solution.java
@@ -1,27 +1,28 @@
 package lv1.한번만등장한문자;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
-    }
+        //문자열리스트에 한문자씩 담기
+        List<String> strlist = new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            strlist.add(s.charAt(i)+"");
+        }
 
-    //v2
-//    String answer = "";
-//
-//    //리스트에 문자 넣기
-//    List<String> strlist = new ArrayList<>();
-//    strlist = Arrays.stream(s.split("")).toList();
-//
-//    //문자 개수 세기
-//        for(int i=0;i<s.length();i++){
-//        int cnt = Collections.frequency(strlist, s.charAt(i)+"");
-//        if(cnt == 1)
-//            answer += s.charAt(i)+"";
-//    }
-//
-//    //문자열 정렬하기
-//    char[] charArr = answer.toCharArray();
-//        Arrays.sort(charArr);
-//        return new String(charArr);
+        //리스트에서 문자 개수를 세고 한개만 있는 문자를 문자열에 추가
+        String answer = "";
+        for(String c:strlist){
+            if(Collections.frequency(strlist,c) == 1)
+                answer += c;
+        }
+
+        //문자열을 정렬한 뒤 반환
+        char[] chs = answer.toCharArray();
+        Arrays.sort(chs);
+        return new String(chs);
+    }
 }
