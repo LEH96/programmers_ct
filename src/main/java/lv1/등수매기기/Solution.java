dedb3e82138724fde9c58545ec548f97dd55d2f4
LEH96 <lutea67@naver.com>
2023-04-07 17:38:52 +0900

    230407 commit

--- src/main/java/lv1/등수매기기/Solution.java
+++ src/main/java/lv1/등수매기기/Solution.java
@@ -1,4 +1,34 @@
 package lv1.등수매기기;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
+    public int[] solution(int[][] score) {
+        int[] rank = new int[score.length];
+        for(int i=0;i<score.length;i++){
+            rank[i] = (score[i][0] + score[i][1]) / 2;
+        }
+        Arrays.sort(rank);
+
+        List<Integer> rankDict = new ArrayList<>();
+        for(int i : rank){
+            if(){
+                if(!rankDict.contains(i)){
+                    rank
+                }
+            }
+        }
+        rankDict = {}
+        for i in range(len(rank)):
+        if rank.count(rank[i]) > 1:
+        if rank[i] not in rankDict:
+        rankDict[rank[i]] = i + 1
+            else:
+        rankDict[rank[i]] = i + 1
+
+        return [rankDict[sum(s) / 2] for s in score]
+    }
 }
--- src/main/java/lv1/특이한정렬/Solution.java
+++ src/main/java/lv1/특이한정렬/Solution.java
@@ -1,4 +1,10 @@
 package lv1.특이한정렬;
 
 public class Solution {
+    public int[] solution(int[] numlist, int n) {
+
+        diff = sorted([(abs(num - n), -num) for num in numlist])
+        answer = [-diff[i][1] for i in range(len(numlist))]
+        return answer
+    }
 }
