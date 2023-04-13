1273e3b422bf59d9cafb44f8b74946658796ff1c
LEH96 <lutea67@naver.com>
2023-04-13 16:22:38 +0900

    230413 commit

--- src/main/java/lv1/겹치는선분의길이/Solution.java
+++ src/main/java/lv1/겹치는선분의길이/Solution.java
@@ -1,4 +1,27 @@
 package lv1.겹치는선분의길이;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 public class Solution {
+    public int solution(int[][] lines) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] line : lines) {
+            int a = line[0];
+            int b = line[1];
+            for (int k = a; k < b; k++) {
+                result.add(k);
+            }
+        }
+
+        HashSet<Integer> numsOfResult = new HashSet<>(result);
+        int sum = 0;
+        for(int num : numsOfResult){
+            if(Collections.frequency(numsOfResult, num) > 1)
+                sum++;
+        }
+        return sum;
+    }
 }
